8cb0759ea0195b1b742525a11d766fc1509965e1
Sasha Melnyk <alex.p.melnyk@gmail.com>
2022-10-07 22:35:00 +0300

    ESHOP-1 upgraded getLastFiveItems method, separated changeLog files

--- src/main/java/com/alex/eshop/eshop/ExceptionHandling/NoSuchItemException.java
+++
@@ -1,7 +0,0 @@
-package com.alex.eshop.eshop.ExceptionHandling;
-
-public class NoSuchItemException extends RuntimeException{
-    public NoSuchItemException(String message){
-        super(message);
-    }
-}
