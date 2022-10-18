668409bd4fab1e437197106c111142c342a61cdf
Sasha Melnyk <alex.p.melnyk@gmail.com>
2022-10-18 16:45:02 +0300

    ESHOP-2 deleted OptionalUtils.java

--- src/main/java/com/alex/eshop/eshop/OptionalUtils.java
+++
@@ -1,9 +0,0 @@
-package com.alex.eshop.eshop;
-
-import java.util.Optional;
-
-public class OptionalUtils {
-    public static <T> T fromOptional(Optional<T> optional) {
-        return optional.orElse( null );
-    }
-}
