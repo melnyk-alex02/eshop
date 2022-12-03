26277a85a449cf4cc668a9fdf53dfd395d86047b
Melnyk <alex.p.melnyk@gmail.com>
2022-12-03 20:17:12 +0200

    ESHOP-5 simplifized countItemsByCategory in CategoryRepository

--- src/main/java/com/alex/eshop/repository/CategoryRepository.java
+++ src/main/java/com/alex/eshop/repository/CategoryRepository.java
@@ -8,6 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    @Query("select count(i.category.id) from Item i where i.category.id =:id")
-    Long countItemsByCategory(Long id);
+    @Query("select count(i) from Item i where i.category.id =:id")
+    long countItemsByCategory(Long id);
 }
