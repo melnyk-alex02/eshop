a7dc8354f70e68a751b857f38cd556ffec0732d5
Melnyk <alex.p.melnyk@gmail.com>
2022-12-12 00:06:32 +0200

    ESHOP-7 added ApplicationProperties, KeycloakClientFactory,KeycloakService, UserService and Controller

---
+++ src/main/java/com/alex/eshop/restcontroller/UserController.java
@@ -0,0 +1,23 @@
+package com.alex.eshop.restcontroller;
+
+import com.alex.eshop.service.UserService;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @GetMapping("users/{Uuid}")
+    public UserRepresentation getUser(@PathVariable String Uuid) {
+        return userService.getUserRepresentation(Uuid);
+    }
+}
